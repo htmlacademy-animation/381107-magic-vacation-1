6804af4a236c07ba0a5c764bf2206e32a2708a12
marina.kuznetsova <marina.kuznetsova@piano.io>
2020-05-02 01:18:42 +0400

    Implement minor fixes according to Travis requirements

--- source/js/modules/body.js
+++ source/js/modules/body.js
@@ -1,7 +1,7 @@
 export default () => {
   let body = document.body;
 
-  window.addEventListener("load", () => {
-    body.classList.add("body-ready");
+  window.addEventListener(`load`, () => {
+    body.classList.add(`body-ready`);
   });
 };
